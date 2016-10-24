fca97b1e1efbf2388575cd90dc3bb12e99532023
yuanyang <316537908@qq.com>
2016-10-24 17:16:26 +0800

    first co

---
+++ README.md
@@ -0,0 +1,45 @@
+# MTCNN_face_detection_and_alignment
+
+## About
+
+  This is a python/mxnet implementation of [Zhang](https://kpzhang93.github.io/)'s work **<Joint Face Detection and Alignment using Multi-task Cascaded Convolutional Neural Networks>**. it's fast and accurate,  see [link](https://github.com/kpzhang93/MTCNN_face_detection_alignment). 
+
+  It should have **almost** the same output with the original work,  for mxnet fans and those can't afford matlab :)
+
+[中文blog]()
+
+## Requirement	  
+
+- opencv 
+
+  ​	I use cv2 for image io and resize(much faster than skimage), the input image's channel is acutally BGR
+
+- mxnet 
+
+  ​	**please update to the newest version, we need 'full' mode in Pooling operation**
+
+Only tested on Linux and Mac
+
+## Test
+
+run:
+
+ ``python main.py`` 
+
+you can change `ctx` to `mx.gpu(0)` for faster detection
+
+## Results
+
+![big4](http://7xsc78.com1.z0.glb.clouddn.com/face_mtcnn.png)
+
+
+
+## License
+
+MIT LICENSE
+
+
+
+## Reference
+
+K. Zhang and Z. Zhang and Z. Li and Y. Qiao Joint,  Face Detection and Alignment Using Multitask Cascaded Convolutional Networks, IEEE Signal Processing Letters
---
+++ main.py
@@ -0,0 +1,61 @@
+# coding: utf-8
+import mxnet as mx
+from mtcnn_detector import MtcnnDetector
+import cv2
+import os
+import time
+
+detector = MtcnnDetector(model_folder='model', ctx=mx.cpu(0))
+
+
+img = cv2.imread('test2.jpg')
+
+t1 = time.time()
+results = detector.detect_face(img, False)
+print 'time: ',time.time() - t1
+
+if results is not None:
+
+    total_boxes = results[0]
+    points = results[1]
+
+    draw = img.copy()
+    for b in total_boxes:
+        cv2.rectangle(draw, (int(b[0]), int(b[1])), (int(b[2]), int(b[3])), (255, 255, 255))
+
+    for p in points:
+        for i in range(5):
+            cv2.circle(draw, (p[i], p[i + 5]), 1, (0, 0, 255), 2)
+
+    cv2.imshow("detection result", draw)
+    cv2.waitKey(0)
+
+# --------------
+# test on camera
+# --------------
+'''
+camera = cv2.VideoCapture(0)
+while True:
+    grab, frame = camera.read()
+    img = cv2.resize(frame, (320,180))
+
+    t1 = time.time()
+    results = detector.detect_face(img, False)
+    print 'time: ',time.time() - t1
+
+    if results is None:
+        continue
+
+    total_boxes = results[0]
+    points = results[1]
+
+    draw = img.copy()
+    for b in total_boxes:
+        cv2.rectangle(draw, (int(b[0]), int(b[1])), (int(b[2]), int(b[3])), (255, 255, 255))
+
+    for p in points:
+        for i in range(5):
+            cv2.circle(draw, (p[i], p[i + 5]), 1, (255, 0, 0), 2)
+    cv2.imshow("detection result", draw)
+    cv2.waitKey(30)
+'''
